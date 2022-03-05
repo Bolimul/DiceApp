c0ff0cf37a40d29d758e745a2ee895155f89be37
Bolimul <nikita.solonets@gmail.com>
2022-03-05 18:54:28 +0200

    onClick method is more general now. There is two TypedArrays to hold resources such as drawable and id's. If the addition of additional image resource or ImageView is needed, there's no need to change the code itself. Only to add in arrays.xml in items in according to your needs.

--- app/src/main/java/com/example/diceapp/MainActivity.java
+++ app/src/main/java/com/example/diceapp/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.diceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
 
 import android.content.res.TypedArray;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button diceButton;
+    TypedArray dicesTypes;
     TypedArray dices;
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +28,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         diceButton = findViewById(R.id.diceButton);
         diceButton.setOnClickListener(this);
 
-        dices = getResources().obtainTypedArray(R.array.diceList);
+
+        dicesTypes = getResources().obtainTypedArray(R.array.diceTypeList);
+        dices = getResources().obtainTypedArray(R.array.dicesImageViews);
+        mp = MediaPlayer.create(this, R.raw.dice_sound);
     }
 
     @Override
     public void onClick(View v)
     {
-        ImageView firstDice = findViewById(R.id.firstDice);
-        ImageView secondDice = findViewById(R.id.secondDice);
         Random rand = new Random();
-        int firstRandomNum = rand.nextInt(dices.length()-1) + 1;
-        int secondRandomNum = rand.nextInt(dices.length()-1) + 1;
-        firstDice.setImageDrawable(AppCompatResources.getDrawable(this, dices.getResourceId(firstRandomNum, 0)));
-        secondDice.setImageDrawable(AppCompatResources.getDrawable(this, dices.getResourceId(secondRandomNum, 0)));
+        for (int i = 0; i < dices.length(); ++i)
+        {
+            ImageView dice = findViewById(dices.getResourceId(i,0));
+            dice.setImageResource(dicesTypes.getResourceId(rand.nextInt(6), 0));
+            YoYo.with(Techniques.Shake).duration(500).repeat(1).playOn(dice);
+        }
+        mp.start();
     }
-
-
 }
